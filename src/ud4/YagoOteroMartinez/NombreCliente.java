1e3b82d42ad1544a83a1dcbdb1f1ff563544b904
Yago Otero <yagootero2003@gmail.com>
2025-04-08 12:29:24 +0200

    Añadido el paquete , Yago Otero Martínez

---
+++ src/ud4/YagoOteroMartinez/NombreCliente.java
@@ -0,0 +1,13 @@
+package ud4.a11maloscheiros.Yago_Otero_Martinez;
+
+// Ejemplo de clase muy básica
+public class NombreCliente {
+
+    String nombre;
+
+    public NombreCliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
